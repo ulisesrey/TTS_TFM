90c3401ef967002d63649b369f3553acc88bad73
ulisesrey <ulises.rey.torne@gmail.com>
2025-05-19 12:43:30 +0200

    in progress: improving evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,7 +3,7 @@ import numpy as np
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 
-def compute_mcd(y_true, y_pred, sr=22050):
+def compute_mcd(y_true, y_pred, sr=16000):
     # Extract MFCCs
     mfcc_true = librosa.feature.mfcc(y=y_true, sr=sr, n_mfcc=13)
     mfcc_pred = librosa.feature.mfcc(y=y_pred, sr=sr, n_mfcc=13)
@@ -14,7 +14,16 @@ def compute_mcd(y_true, y_pred, sr=22050):
     # Return average distance per aligned frame
     return distance / len(path)
 
-y_true, sr = librosa.load("data/audio/arg_female_5_true.wav", sr=22050)
-y_pred, sr = librosa.load("data/audio/arg_female_5_pred.wav", sr=22050)
-mcd = compute_mcd(y_true, y_pred)
-print(f"MCD: {mcd}")
+def evaluation_wrapper(data_path, output_path):
+    # Load the true and predicted audio files
+    for file in os.listdir(data_path):
+        if file.endswith(".wav"):
+            y_true_path = os.path.join(data_path, file)
+            y_pred_path = os.path.join(output_path, file)
+            break
+    y_true, sr = librosa.load(y_true_path, sr=16000)
+    y_pred, sr = librosa.load(y_pred_path, sr=16000)
+    mcd = compute_mcd(y_true, y_pred)
+    return mcd
+# Print the MCD with 3 decimal points
+print(f"MCD: {mcd:.3f}")
